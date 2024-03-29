bc0c2d25a6d5f527a0e298a450911c2b0e4a879a
Diogo Berner <diogoberner@gmail.com>
2024-03-29 18:46:44 -0300

    feat: navbar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Logo from "../Logo";
 
 import "./Navbar.css";
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <Link to="/" className="logo">
@@ -11,16 +13,25 @@ const Navbar = () => {
       </Link>
       <div className="white-line"></div>
       <nav className="nav">
-        <Link to="/">
+        <Link to="/" className={location.pathname === "/" ? "active" : ""}>
           <span>00</span> Home
         </Link>
-        <Link to="/destination">
+        <Link
+          to="/destination"
+          className={location.pathname === "/destination" ? "active" : ""}
+        >
           <span>01</span> Destination
         </Link>
-        <Link to="/crew">
+        <Link
+          to="/crew"
+          className={location.pathname === "/crew" ? "active" : ""}
+        >
           <span>02</span> Crew
         </Link>
-        <Link to="/technology">
+        <Link
+          to="/technology"
+          className={location.pathname === "/technology" ? "active" : ""}
+        >
           <span>03</span> Technology
         </Link>
       </nav>
